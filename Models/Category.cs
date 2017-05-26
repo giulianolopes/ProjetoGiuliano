0ad293e84bd3b6ff8ea965cf4797b54bbfad6a8f
giulianolopes <giulianolopes_18@hotmail.com>
2017-05-26 20:43:57 -0300

    Add files via upload

---
+++ Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace Giuliano.Paula.Models
+{
+    public class Category
+    {
+        public long CategoryId { get; set; }
+        public string Name { get; set; }
+
+        public virtual ICollection<Product> Products { get; set; }
+    }
+}
---
+++ Models/Supplier.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Giuliano.Paula.Models
+{
+    public class Supplier
+    {
+        public long SupplierId { get; set; }
+        public string Name { get; set; }
+
+        public virtual ICollection<Product> Products { get; set; }
+
+
+    }
+}
